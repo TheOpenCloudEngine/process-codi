40fcd5520bc5e4b873d6fcbcbb8eadfffede18eb
unknown <limhoo@uengine.org>
2015-08-06 17:47:30 +0900

    다가오는 일정 안 보이는 문제 해결.

--- src/main/java/org/uengine/codi/mw3/model/Perspective.java
+++ src/main/java/org/uengine/codi/mw3/model/Perspective.java
@@ -36,7 +36,7 @@ public class Perspective {
 	public final static String USE_ROLE = GlobalContext.getPropertyString("role.use", "1");
 	public final static String USE_APP = GlobalContext.getPropertyString("app.use", "0");
 	public final static String USE_CONTACT = GlobalContext.getPropertyString("contact.use", "1");
-	public final static String USE_COMMINGTODO = GlobalContext.getPropertyString("commingTodo.use", "0");
+	public final static String USE_COMMINGTODO = GlobalContext.getPropertyString("commingTodo.use", "1");
 	public final static String USE_PERSPECTIVE_KNOWLEDGE = GlobalContext.getPropertyString("perspective.knowledge.use", "0");
 	public final static String USE_DOCUMENT = GlobalContext.getPropertyString("document.use", "0");
 	public final static String USE_GROUP		= GlobalContext.getPropertyString("group.use", "1");
