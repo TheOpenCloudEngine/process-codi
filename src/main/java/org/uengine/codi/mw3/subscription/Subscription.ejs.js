2213023a4b18132f5326d7f51cede291d1b7c561
basquiat <funnyjazz@naver.com>
2016-06-22 11:35:55 +0900

    style add

--- src/main/java/org/uengine/codi/mw3/subscription/Subscription.ejs.js
+++ src/main/java/org/uengine/codi/mw3/subscription/Subscription.ejs.js
@@ -1,4 +1,14 @@
 var org_uengine_codi_mw3_subscription_Subscription = function(objectId, className){
+
+    this.objectId = objectId;
+    this.className = className;
+    this.object = mw3.objects[this.objectId];
+
+    $("#objDiv_"+this.objectId).find(".tbl_th").attr("style","background:#ddd;font-weight:bold;border:1px solid #ccc;border-left:0 none;display:table-cell;width:232px;height:30px;border-right:1px solid #ccc;border-bottom:1px solid #ccc;text-align: center; vertical-align: middle;");
+    $("#objDiv_"+this.objectId).find(".tbl_td").attr("style","display:table-cell;width:232px;height:30px;border-right:1px solid #ccc;border-bottom:1px solid #ccc;text-align: center; vertical-align: middle;");
+    $("#objDiv_"+this.objectId).find(".tbl_th").first().css("width","150px").css("border-left","1px solid #ccc");
+    $("#objDiv_"+this.objectId).find(".tbl_td").first().css("width","150px").css("border-left","1px solid #ccc");
+
     var dataSet = [
         [
             "Tiger Nixon",
