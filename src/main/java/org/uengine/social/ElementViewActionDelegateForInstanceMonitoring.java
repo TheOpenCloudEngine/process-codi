5857354090f0efd83a8e4d710653252ae3628553
admin <admin>
2016-07-20 13:03:14 +0900

    no message

--- src/main/java/org/uengine/social/ElementViewActionDelegateForInstanceMonitoring.java
+++ src/main/java/org/uengine/social/ElementViewActionDelegateForInstanceMonitoring.java
@@ -1,5 +1,6 @@
 package org.uengine.social;
 
+import org.metaworks.annotation.Id;
 import org.metaworks.dwr.MetaworksRemoteService;
 import org.metaworks.widget.Label;
 import org.metaworks.widget.ModalWindow;
@@ -11,6 +12,16 @@ import org.uengine.modeling.ElementViewActionDelegate;
  * Created by jjy on 2015. 11. 6..
  */
 public class ElementViewActionDelegateForInstanceMonitoring implements ElementViewActionDelegate{
+
+    String instanceId;
+    @Id
+        public String getInstanceId() {
+            return instanceId;
+        }
+        public void setInstanceId(String instanceId) {
+            this.instanceId = instanceId;
+        }
+
 
     @Override
     public void onDoubleClick(ElementView elementView) {
